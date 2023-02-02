8ee9e884048d36caef34eb5866fc23b2220ee703
NullDude05 <jdandiaz2021@gmail.com>
2023-02-02 15:02:17 -0600

    first commit

---
+++ MyStore/Pages/Clients/Index.cshtml.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
+
+namespace MyStore.Pages.Clients
+{
+    public class IndexModel : PageModel
+    {
+        public List<ClientInfo> listClients = new List<ClientInfo>();
+        public void OnGet()
+        {
+            try
+            {
+                Console.WriteLine("trying to go through");
+                String connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=mystore;Integrated Security=True";
+
+                using (SqlConnection connection = new SqlConnection(connectionString)) 
+                {
+                    connection.Open();
+                    String sql = "SELECT * FROM clients";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader()) 
+                        {
+                            while(reader.Read()) 
+                            {
+                                ClientInfo clientInfo = new ClientInfo();
+
+                                clientInfo.id = "" + reader.GetInt32(0);
+                                clientInfo.name = reader.GetString(1);
+                                clientInfo.email = reader.GetString(2);
+                                clientInfo.phone = reader.GetString(3);
+                                clientInfo.address = reader.GetString(4);
+                                clientInfo.created_at = reader.GetDateTime(5).ToString();
+
+                                listClients.Add(clientInfo);
+                                Console.WriteLine(clientInfo.name);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
+        }
+    }
+
+    public class ClientInfo
+    {
+        public String id;
+        public String name;
+        public String email;
+        public String phone;
+        public String address;
+        public String created_at;
+    }
+}
